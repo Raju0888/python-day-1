a58d2126141992db561d73c28d018bc1fa4a8d64
Raju0888 <112844343+Raju0888@users.noreply.github.com>
2022-10-07 11:55:33 +0530

    Add files via upload

---
+++ sqare and cube4th.py
@@ -0,0 +1,7 @@
+def square (num):
+  return num*num #Square of a number
+def cube (num):
+  return num*num*num #Cube of a number
+number = int(input("Enter an integer number: "))
+print("Square of ",number," is ", square(number))
+print("Cube of ",number," is ", cube(number))
